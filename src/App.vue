3497f4081bc89d89e8d08092d0d9af7e91f222e9
Jef <incutonez@hotmail.com>
2024-09-22 17:03:31 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import {TestValue} from "@incutonez/my-package/dist/utils";
+import {ComponentA} from "@incutonez/my-package/dist/components";
+
+console.log(TestValue)
+</script>
+
+<template>
+  <ComponentA />
+</template>
